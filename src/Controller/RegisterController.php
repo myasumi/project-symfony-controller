1c809f7ac15ea4387c5fa4cd0ae6ba868567ebcd
myasumi <asumi97@outlook.com>
2019-07-13 10:44:27 -0700

    Se creo la funcion actionFormRegisterTeacher y su respectiva vista de registro

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Departament;
 use App\Entity\Faculty;
 use App\Entity\School;
+use App\Entity\Teacher;
 use App\Entity\Users;
+use App\Form\DepartmentType;
 use App\Form\FacultyType;
 use App\Form\SchoolType;
+use App\Form\TeacherType;
 use App\Form\UserType;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -68,6 +72,42 @@ class RegisterController extends AbstractController
             return $this->redirectToRoute('security_register_school');
         }
         return $this->render('register/school.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/register_department", name="security_register_department")
+     */
+    public function actionFormRegisterDepartment(Request $request, ObjectManager $manager)
+    {
+        $department = new Departament();
+        $form = $this->createForm(DepartmentType::class, $department);
+        $form->handleRequest( $request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($department);
+            $manager->flush();
+            return $this->redirectToRoute('security_register_department');
+        }
+        return $this->render('register/department.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/register_teacher", name="security_register_teacher")
+     */
+    public function registerTeacher(Request $request, ObjectManager $manager)
+    {
+        $teacher = new Teacher();
+        $form = $this->createForm(TeacherType::class, $teacher);
+        $form->handleRequest( $request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($teacher);
+            $manager->flush();
+            return $this->redirectToRoute('security_register_teacher');
+        }
+        return $this->render('register/teacher.html.twig', [
             'form' => $form->createView(),
         ]);
     }
