7ffd54fe883cd39c025977d5fa93e18b5dc27d27
myasumi <asumi97@outlook.com>
2019-07-13 18:46:45 -0700

    Se hizo la configuracion de seguridad en security.yml para la autenticacion del usuario para el ingreso al sistema

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function actionLogin(AuthenticationUtils $authentication)
+    {
+        $error = $authentication->getLastAuthenticationError();
+        $username = $authentication->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            'username' => $username,
+            'error'  => $error,
+        ]);
+    }
+}
