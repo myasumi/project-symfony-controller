69236b542ab2e1566a4e472ec1e25384ddf7bade
Rousee <j.andia@unas.edu.pe>
2019-07-18 23:12:04 -0500

    Se realizo el comentario de la creacion de un formulario de user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -14,6 +14,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+    //Realizamos el formulario de usuarios
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
